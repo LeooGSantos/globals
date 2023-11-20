9af21ac2f6dd3bfc25835fb21a176b62fb928a7b
LeooGSantos <rm98912@fiap.com.br>
2023-11-20 20:39:23 -0300

    Pagina Integração de dados

--- src/Components/Cabecalho/Cabecalho.jsx
+++ src/Components/Cabecalho/Cabecalho.jsx
@@ -14,10 +14,10 @@ export default function Cabecalho() {
           <Link href="/Pages/prevencao">Prevenção e Promoção da Saúde</Link>
         </li>
         <li>
-          <Link href="/Pages/colaboracao">Prevenção e Promoção da Saúde</Link>
+          <Link href="/Pages/colaboracao">Colaboração Eficiente</Link>
         </li>
         <li>
-          <Link href="/Pages/prevencao">Prevenção e Promoção da Saúde</Link>
+          <Link href="/Pages/prevencao">Segurança e Privacidade</Link>
         </li>
         <li className="login-register">
             <Link href="/Pages/login">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="Home">
@@ -6,7 +9,34 @@ export default function Home() {
         Na vanguarda da saúde conectada, o Safe Connect Health oferece mais do que uma plataforma - é uma revolução no cuidado e na gestão da sua saúde. Integramos dados provenientes de múltiplas fontes para proporcionar uma visão abrangente e única da sua saúde.
       </p>
       <div className="benefitsGrid">
-        {/* Benefícios destacados */}
+        <div className="benefit">
+          <h2>Integração Completa</h2>
+          <p>Diferentemente de soluções fragmentadas, integramos dados de diferentes fontes, proporcionando uma visão abrangente da saúde do usuário.</p>
+          <button className="bene">
+          <Link href="/Pages/integracao-dados">Integração de Dados</Link>
+          </button>
+        </div>
+        <div className="benefit">
+          <h2>Enfoque na Prevenção</h2>
+          <p>Destaca-se por suas funcionalidades de prevenção, incentivando hábitos saudáveis e alertando para práticas preventivas.</p>
+          <button className="bene">
+          <Link href="/Pages/prevencao">Prevenção</Link>
+          </button>
+        </div>
+        <div className="benefit">
+          <h2>Colaboração Eficiente</h2>
+          <p>Facilita a colaboração entre pacientes e profissionais de saúde, promovendo um cuidado mais personalizado e eficiente.</p>
+          <button className="bene">
+          <Link href="/Pages/colaboracao">Colaboração Eficiente</Link>
+          </button>
+        </div>
+        <div className="benefit">
+          <h2>Segurança e Privacidade</h2>
+          <p>Implementa protocolos de segurança robustos, garantindo a proteção dos dados do usuário e o cumprimento das regulamentações de privacidade.</p>
+          <button className="bene">
+          <Link href="/Pages/privacidade">Segurança e Privacidade</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
