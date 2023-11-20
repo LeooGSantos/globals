72eabd86795bdeb6b1042ba7d2e0bb209a13a48f
LeooGSantos <rm98912@fiap.com.br>
2023-11-20 13:42:22 -0300

    Estilizando

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,13 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div className="pagina-principal">
-      <h1>Seguro de bicicletas da Porto</h1>
-      <img src="/img/5p.jpg" alt="Imagem2" className="imagem-plano" />
-      <p>Uma solução completa que oferece proteção e serviços para você se aventurar sem medo.</p>
-      <p>Com o Seguro Bike da Porto Seguro, é pedalar e relaxar.</p>
-      <p>Faça seu seguro agora, de forma dinâmica e rápida!</p>
-      <Link href="/Pages/planos-seguro"> 
-        <button>Cote já</button>
-      </Link>
+    <div className="Home">
+      <h1 className="title">Bem-vindo ao Safe Connect Health</h1>
+      <p className="description">
+        Na vanguarda da saúde conectada, o Safe Connect Health oferece mais do que uma plataforma - é uma revolução no cuidado e na gestão da sua saúde. Integramos dados provenientes de múltiplas fontes para proporcionar uma visão abrangente e única da sua saúde.
+      </p>
+      <div className="benefitsGrid">
+        {/* Benefícios destacados */}
+      </div>
     </div>
   );
 }
-
-
-
-
-
-
